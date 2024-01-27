8adbb159cd46ac63db50b2cd273be16c69377899
TheSirTaco7 <114195041+TheSirTaco7@users.noreply.github.com>
2024-01-27 14:47:38 -0500

    I figured it out
    I needed a parse int

--- README.md
+++ README.md
@@ -3,25 +3,25 @@
 
 <br><h2>board.txt</h2>
 <br>board.txt is a text file to show the stating state of the board.
-<br>0: Empty circle 
+<br>0: Empty box 
 <br>1: Red
-<br>2: Dark Red
+<br>2: Green
 <br>3: Blue
-<br>4: Teal
-<br>5: Dark purple
-<br>6: Light Purple
-<br>7: Green
+<br>4: Purple
+<br>5: Orange
+<br>6: Teal
+<br>7: Light Purple
 <br>8: Yellow
-<br>9: Orange
+<br>9: Dark Red
 
 <h3>Example:</h3>
 <img src="exampleFlow.jpg" alt="exampleFlow" width="200"/>
 
 Would be:
 
-1 0 7 0 5
-<br>0 0 3 0 9
+1 0 2 0 4
+<br>0 0 3 0 5
 <br>0 0 0 0 0
-<br>0 7 0 5 0
-<br>0 1 3 9 0
+<br>0 2 0 4 0
+<br>0 1 3 5 0
 
--- solver.java
+++ solver.java
@@ -4,55 +4,64 @@ class Main {
     public static void main(String[] args) {
 
         int[][] board = readInBoard();
-        System.out.println(board.length);
-        System.out.println(board[0].length);
-
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                System.out.print(board[i][j]);
-            }
-            System.out.println();
-        }
+        System.out.println(totalPaths(board));
 
     }
 
     public static int[][] readInBoard() {
 
         int width = 1;
-        int hight = 1;
+        int height = 1;
 
         
         try{
     
-            BufferedReader baordReader = new BufferedReader(new FileReader("board.txt"));
+            BufferedReader boardReader = new BufferedReader(new FileReader("board.txt"));
 
-            //Get hight
+            //Get height
             int lines = 0;
-            while (baordReader.readLine() != null) lines++;
-            hight = lines;
+            while (boardReader.readLine() != null) lines++;
+            height = lines;
 
             //Get width
-            baordReader.close();
-            baordReader = new BufferedReader(new FileReader("board.txt"));
-            width = (new String(baordReader.readLine()).length() + 1) / 2;
+            boardReader.close();
+            boardReader = new BufferedReader(new FileReader("board.txt"));
+            //width = (new String(boardReader.readLine()).length() + 1) / 2;
+            width = boardReader.readLine().split(" ").length;
 
-            int[][] boardVaraiable = new int[width][hight];
+            int[][] boardVariable = new int[width][height];
 
-            baordReader.close();
-            baordReader = new BufferedReader(new FileReader("board.txt"));
+            boardReader.close();
+            boardReader = new BufferedReader(new FileReader("board.txt"));
 
-            for(int i = 0; i < width; i++){
-                for(int j = 0; j < hight; j++){
-                    boardVaraiable[i][j] = baordReader.read();
-                    baordReader.read();
+            for (int i = 0; i < height; i++) {
+                String[] values = boardReader.readLine().split(" ");
+                for (int j = 0; j < width; j++) {
+                    boardVariable[i][j] = Integer.parseInt(values[j]);
                 }
             }
 
-            return boardVaraiable;
+            return boardVariable;
 
         } catch (Exception e) {}
 
         return new int[0][0];
+
+    }
+
+    public static int totalPaths(int[][] board){
+
+        int max = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] > max){
+                    max = board[i][j];
+                }
+            }
+        }
+
+        return max;
 
     }
 
